46d7396f7794a6ef43b1fc9b5d1ad6d7151cc127
Arash Jangali <arashjangali@Arashs-MacBook-Pro.local>
2023-09-06 21:27:34 +0200

    stripe-webhook

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel')
 const moment = require('moment')
 
 // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-const stripe = require('stripe')(process.env.STRIPE_TEST_KEY)
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 
 
