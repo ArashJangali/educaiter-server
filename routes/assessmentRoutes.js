95a74b0257d15a1b2f1d3185809694fdf96a9665
Arash Jangali <arashjangali@Arashs-MacBook-Pro.local>
2023-10-28 23:57:20 +0200

    small change quiz route

--- routes/assessmentRoutes.js
+++ routes/assessmentRoutes.js
@@ -1,55 +1,105 @@
-const express = require('express');
-const { check, validationResult } = require('express-validator');
+const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
-const { getAssessmentsByUserId, generateQuestion, evaluateAnswer, lineChart } = require('../controllers/assessmentController');
-const authenticateToken = require('../middleware/authenticateToken')
-const userIdValidation = require('../middleware/userIdValidation')
-const { checkSubscriptionExists, usageLimit } = require('../middleware/checkSubscription')
-const User = require('../models/userModel')
+const {
+  getAssessmentsByUserId,
+  generateQuestion,
+  evaluateAnswer,
+  lineChart,
+} = require("../controllers/assessmentController");
+const authenticateToken = require("../middleware/authenticateToken");
+const userIdValidation = require("../middleware/userIdValidation");
+const {
+  checkSubscriptionExists,
+  usageLimit,
+} = require("../middleware/checkSubscription");
+const User = require("../models/userModel");
 
 const rateLimit = require("express-rate-limit");
 
-
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  });
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
 
-
-
-
-router.get('/generateQuestion/:topic/:level',
-check('topic').isIn(['Computer Science', 'Mathematics', 'Design', "Digital Marketing", "Law", "Finance", "Entrepreneurship", "Accounting", "Life Science", "Physical Science", "Philosophy", "Psychology"]),
-check('level').isIn(['Beginner', 'Intermediate', 'Advanced']),
-authenticateToken, limiter, usageLimit, generateQuestion);
-
-router.post('/evaluateAnswer/:topic/:level', 
-  check('topic').isIn([
-    "Software Development", 
-    "Web Development", 
-    "Mobile App Development", 
-    "Data Science & Analytics", 
-    "Machine Learning & AI", 
-    "Cloud Computing & DevOps", 
-    "Cybersecurity", 
-    "Mathematics", 
-    "Blockchain & Cryptocurrency", 
-    "Quantum Computing", 
-    "Augmented & Virtual Reality (AR/VR)", 
-    "Internet of Things (IoT)"
-  ]), 
-  check('level').isIn(['Foundational', 'Undergraduate', 'Postgraduate', 'Doctorate', 'Researcher', 'Expert', 'Master', 'Visionary']), 
-  authenticateToken, limiter, usageLimit, evaluateAnswer
+router.get(
+  "/generateQuestion/:topic/:level",
+  check("topic").isIn([
+    "Software Development",
+    "Web Development",
+    "Mobile App Development",
+    "Data Science & Analytics",
+    "Machine Learning & AI",
+    "Cloud Computing & DevOps",
+    "Cybersecurity",
+    "Mathematics",
+    "Blockchain & Cryptocurrency",
+    "Quantum Computing",
+    "Augmented & Virtual Reality (AR/VR)",
+    "Internet of Things (IoT)",
+  ]),
+  check("level").isIn([
+    "Foundational",
+    "Undergraduate",
+    "Postgraduate",
+    "Doctorate",
+    "Researcher",
+    "Expert",
+    "Master",
+    "Visionary",
+  ]),
+  authenticateToken,
+  limiter,
+  usageLimit,
+  generateQuestion
 );
 
+router.post(
+  "/evaluateAnswer/:topic/:level",
+  check("topic").isIn([
+    "Software Development",
+    "Web Development",
+    "Mobile App Development",
+    "Data Science & Analytics",
+    "Machine Learning & AI",
+    "Cloud Computing & DevOps",
+    "Cybersecurity",
+    "Mathematics",
+    "Blockchain & Cryptocurrency",
+    "Quantum Computing",
+    "Augmented & Virtual Reality (AR/VR)",
+    "Internet of Things (IoT)",
+  ]),
+  check("level").isIn([
+    "Foundational",
+    "Undergraduate",
+    "Postgraduate",
+    "Doctorate",
+    "Researcher",
+    "Expert",
+    "Master",
+    "Visionary",
+  ]),
+  authenticateToken,
+  limiter,
+  usageLimit,
+  evaluateAnswer
+);
 
+router.get(
+  "/:userId",
+  authenticateToken,
+  limiter,
+  userIdValidation,
+  getAssessmentsByUserId
+);
 
-
-router.get('/:userId', authenticateToken, limiter, userIdValidation, getAssessmentsByUserId);
-
-
-router.get('/linechart/:userId', authenticateToken, limiter, userIdValidation, lineChart);
-
-
+router.get(
+  "/linechart/:userId",
+  authenticateToken,
+  limiter,
+  userIdValidation,
+  lineChart
+);
 
 module.exports = router;
